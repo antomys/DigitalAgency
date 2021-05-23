4d3dae7ddc163c7ddbb140d772a6fd685cd00038
ihor.volokhovych <ihor.volokhovych@pm.bet>
2021-05-23 20:27:38 +0300

    Bugfix.

---
+++ src/DigitalAgency.Blazor.Front/LoginService/LoginViewModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DigitalAgency.Blazor.Front.LoginService
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "Too long!")]
+        public string UserName { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+    }
+}
